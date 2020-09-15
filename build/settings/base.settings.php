2f9a9ce45582b2db0e31f9a594c1a4a62181e211
camilocodes <camilocodes@gmail.com>
2020-09-15 15:51:25 -0300

    VOI-2 Local changes for D7 migration

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -12,3 +12,14 @@ $settings['install_profile'] = 'minimal';
 $settings['cache_prefix']['default'] = 'voi_';
 $conf['chq_redis_cache_enabled'] = TRUE;
 require_once dirname(__FILE__) . "/settings.redis.inc";
+
+// For migration.
+$databases['migrate']['default'] = array (
+  'database'  => 'voi',
+  'username'  => 'root',
+  'password'  => 'import',
+  'host'      => 'mysqlimport',
+  'port'      => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver'    => 'mysql',
+);
