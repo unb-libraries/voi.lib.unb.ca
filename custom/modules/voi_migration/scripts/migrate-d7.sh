d6cc3e4507fc4f052cb97c6b1a62fd8dade1c80d
camilocodes <camilocodes@gmail.com>
2020-09-17 16:15:50 -0300

    VOI-2 Taxonomy settings, vocabularies, update migration script

--- custom/modules/voi_migration/scripts/migrate-d7.sh
+++ custom/modules/voi_migration/scripts/migrate-d7.sh
@@ -4,18 +4,8 @@
 drush en --yes voi_migration
 
 # Run selected migrations.
-drush migrate-import locale_settings
-drush migrate-import language
-drush migrate-import default_language
 drush migrate-import d7_node_type
-drush migrate-import d7_language_content_settings
-drush migrate-import d7_language_negotiation_settings
-drush migrate-import file_settings
-drush migrate-import d7_user_role
 drush migrate-import d7_user
-drush migrate-import d7_language_types
-drush migrate-import d7_system_site_translation
-drush migrate-import d7_menu
 drush migrate-import taxonomy_settings
 drush migrate-import d7_taxonomy_vocabulary
 drush migrate-import d7_language_content_taxonomy_vocabulary_settings
@@ -33,4 +23,3 @@ drush migrate-import d7_node_complete:page
 drush migrate-import d7_node_translation:document
 drush migrate-import d7_node_translation:page
 drush migrate-import d7_url_alias
-drush migrate-import d7_rdf_mapping
