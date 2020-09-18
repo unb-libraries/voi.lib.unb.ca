63488cb7fee6d1b74b68c7777676fb1825ba1e7c
camilocodes <camilocodes@gmail.com>
2020-09-18 15:30:31 -0300

    VOI-2 Content types and fields (config)

--- custom/modules/voi_migration/scripts/migrate-d7.sh
+++ custom/modules/voi_migration/scripts/migrate-d7.sh
@@ -4,20 +4,17 @@
 drush en --yes voi_migration
 
 # Run selected migrations.
+drush migrate-import language
 drush migrate-import d7_user_role
 drush migrate-import d7_user
 drush migrate-import d7_node_type
-drush migrate-import taxonomy_vocabulary
+drush migrate-import d7_taxonomy_vocabulary
 drush migrate-import d7_taxonomy_term:article_type
 drush migrate-import d7_taxonomy_term:newspapers
-drush migrate-import d7_taxonomy_term:tags
 drush migrate-import d7_taxonomy_term:language
 drush migrate-import d7_taxonomy_term_localized_translation
 drush migrate-import d7_taxonomy_term_translation
-drush migrate-import d7_node:document
-drush migrate-import d7_node:page
+drush migrate-import d7_language_content_settings
 drush migrate-import d7_node_complete:document
 drush migrate-import d7_node_complete:page
-drush migrate-import d7_node_translation:document
-drush migrate-import d7_node_translation:page
 drush migrate-import d7_url_alias
