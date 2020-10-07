e4b6317526ff51bc74e791352269137acaf82b3b
camilocodes <camilocodes@gmail.com>
2020-10-07 12:22:47 -0300

    VOI-2 Add separate script to import UI translation only

---
+++ custom/modules/voi_migration/scripts/ui-translate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Enable migration modules.
+printf "\n\n[UI translation setup for voi.lib.unb.ca]\n\n"
+
+# Import UI translations.
+printf "\n\nImporting UI translations...\n\n"
+drush langimp --langcode=fr /app/html/modules/custom/voi_migration/config/lang/all-fr.po
